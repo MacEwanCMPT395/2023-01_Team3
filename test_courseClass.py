f6d11b6f0e0f3f304eec0ac1c1d7ae8138a64d1a
tadrosv <81656715+tadrosv@users.noreply.github.com>
2023-03-05 18:16:12 -0700

    Updates test_courseClass

--- test_courseClass.py
+++ test_courseClass.py
@@ -63,21 +63,21 @@ class TestStudentSchedule(unittest.TestCase):
                 Term("Term 3", 3)]
         schedule = Schedule(student, degree, program, courses, classrooms, term)
 
-    #     print(f"\n===================================")
-    #     print(f"\n=====Testing check_availability=====\n")
+        print(f"\n===================================")
+        print(f"\n=====Testing check_availability=====\n")
 
-    #     # Test when there is no course scheduled on the given day
-    #     self.assertTrue(schedule.check_availability(courses[0], "Monday", classrooms[0]))
+        # Test when there is no course scheduled on the given day
+        self.assertTrue(schedule.check_availability(courses[0], "Monday", classrooms[0]))
 
-    #     schedule.schedule_course(courses[1], "Monday", classrooms[0]) #9.5
-    #     schedule.schedule_course(courses[2], "Monday", classrooms[0]) #11
-    #     schedule.schedule_course(courses[4], "Monday", classrooms[0]) #14
-    #     schedule.schedule_course(courses[3], "Monday", classrooms[0]) #15.5
-    #     schedule.schedule_course(courses[0], "Monday", classrooms[0]) #17
+        schedule.schedule_course(courses[1], "Monday", classrooms[0]) #9.5
+        schedule.schedule_course(courses[2], "Monday", classrooms[0]) #11
+        schedule.schedule_course(courses[4], "Monday", classrooms[0]) #14
+        schedule.schedule_course(courses[3], "Monday", classrooms[0]) #15.5
+        schedule.schedule_course(courses[0], "Monday", classrooms[0]) #17
 
-    #     # Test when course cannot fit in time slot
-    #     self.assertFalse(schedule.check_availability(courses[1], "Monday", classrooms[0]))
-    #     print(f"===================================")
+        # Test when course cannot fit in time slot
+        self.assertFalse(schedule.check_availability(courses[1], "Monday", classrooms[0]))
+        print(f"===================================")
 
 
     def test_schedule_course(self):
@@ -155,9 +155,9 @@ class TestStudentSchedule(unittest.TestCase):
 
         schedule.replace_course(terms[0], classrooms)
 
-    #     schedule.display_schedule(classrooms[1])
-    #     print("\n")
-    #     print(f"===================================")
+        schedule.display_schedule(classrooms[1])
+        print("\n")
+        print(f"===================================")
 
 
     def test_fill_empty_spot(self):
@@ -190,8 +190,8 @@ class TestStudentSchedule(unittest.TestCase):
                                     "BCOM" : [Course("PRDV ####", "PCOM", 20, 1, 14, 1.5, 0),
                                             Course("FODDER $$$$", "PCOM", 40, 1, 40, 3, 0)]}
 
-    #     print(f"\n===================================")
-    #     print(f"\n=====Testing fill_empty_spot=====\n")
+        print(f"\n===================================")
+        print(f"\n=====Testing fill_empty_spot=====\n")
 
         schedule.schedule_course(courses[1], "Monday", classrooms[1])
         schedule.schedule_course(courses[2], "Monday", classrooms[1])
@@ -201,14 +201,14 @@ class TestStudentSchedule(unittest.TestCase):
         
         schedule.replace_course(terms[0], classrooms)
         
-    #     schedule.display_schedule(classrooms[1])
-    #     print("\n")
+        schedule.display_schedule(classrooms[1])
+        print("\n")
         
         schedule.fill_empty_spot(terms[0], classrooms)
 
-    #     schedule.display_schedule(classrooms[1])
-    #     print("\n")
-    #     print(f"===================================")
+        schedule.display_schedule(classrooms[1])
+        print("\n")
+        print(f"===================================")
 
 
 if __name__ == '__main__':
