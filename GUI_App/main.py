1853c4a0fdded7a1d092a8162d33e01a3bee5698
grahamkhoury <khouryg@mymacewan.ca>
2023-03-28 02:36:36 -0600

    Failure Reasons not displayed

--- GUI_App/main.py
+++ GUI_App/main.py
@@ -665,10 +665,14 @@ class MainWindow(QMainWindow):
     #=======================================================
     #================= Display failed classes ==============
     def display_failed_classes(self, list):
-        self.ui.text_534.append("The following courses could not be scheduled due to capacity issues:\n")
+        self.ui.text_534.append("The following courses could not be scheduled in their entirety due to capacity issues:\n")
+        out_string = ""
+        failed_reasons = ["Too many hours for the semester (expand date range or reduce course hours)",
+                          "Not enough room in the schedule (add more classrooms)",
+                          "Could not create enough cohorts due to physical class bottleneck (add more classrooms)"]
         for item in list:
-            str_item = [str(c) for c in item] # Convert all elements to strings
-            self.ui.text_534.append('\n'.join(str_item))
+            out_string += f"{item[0]}:\n{failed_reasons[item[1]-1]}\n"
+        self.ui.text_534.append(out_string)
 
     #=======================================================
     #================= Display online classes ==============
